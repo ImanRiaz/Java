3f3c238b05a9196fdf7b3b8e2d72ae63f9f3d0ac
ImanRiaz <mani7889khan@gmail.com>
2025-03-12 11:38:37 +0500

    correction

--- TempertaureConverter.java
+++ TempertaureConverter.java
@@ -1,36 +1,32 @@
 class tempt{
     private double temperature;
-        double value;
+        
 
-    void setCelsius(double value){
-        temperature=value;
+   tempt(double value){
+        this.temperature=value;
     }      
 
-    void setFarenhite(double value){
-        temperature=value;
+    void setFarenhite(){
+       temperature = (temperature - 32) * 5 / 9;
     }
 
-    void toFarenhite(){
-        value=((value)*9/5)+32;
+    double toFarenhite(){
+      return  temperature=((temperature)*9/5)+32;
     }
 
-    void toCelsius(){
-        temperature=((temperature)*9/5)+32;
+    double toCelsius(){
+       return temperature=((temperature)*9/5)+32;
     }
 
     void showTemperature(){
-        System.out.println("The celsius value is: "+value+" *C");
-        System.out.println("The Fraenhite is "+temperature+" *F");
+        System.out.println("The celsius value is: "+ toCelsius() +" *C");
+        System.out.println("The Fraenhite is "+toFarenhite()+" *F");
 }
 }
 public class TempertaureConverter {
     public static void main(String [] args)
     {
-        tempt t=new tempt();
-        t.setCelsius(75.23);
-        t.setFarenhite(75.23);
-        t.toCelsius();
-        t.toFarenhite();
+        tempt t=new tempt(25);
     t.showTemperature();
 
     }
